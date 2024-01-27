e450a19873b66174c918908e1cf358420f525bc5
dotorihanjum <150108721+dotorihanjum@users.noreply.github.com>
2024-01-27 18:03:10 +0900

    rename file path

--- code/map_circle.py
+++ code/map_circle.py
@@ -23,7 +23,7 @@ print(year+"_"+count_or_weight)
 
 main_data = 'year_'+year+'_'+count_or_weight
 
-geojson_file = 'TL_SCCO_SIG.json'
+geojson_file = '../DataBase/TL_SCCO_SIG.json'
 gdf = gpd.read_file(geojson_file)
 
 center_lat, center_lon = 36.5, 127.5
@@ -32,7 +32,7 @@ map_korea = folium.Map(location=[center_lat, center_lon], zoom_start=7)
 
 folium.GeoJson(gdf).add_to(map_korea)
 
-Data = 'trash_location.csv'
+Data = '../DataBase/trash_location.csv'
 df = pd.read_csv(Data)
 SCM = ['지역',main_data,'str_la','str_lo','end_la','end_lo']
 df = df[df['str_la'].notnull()] #위도,경도 없는 지역 거르기
@@ -51,9 +51,6 @@ for col, row in df.iterrows():
     #folium.CircleMarker(location=[circle_lat, circle_lon], radius=1, color='black', fill=True, fill_color='black').add_to(map_korea)
     #원 중앙에 점을 찍는 코드. 시각적으로 뭐가 나은지 모르겠어서 남겨놓음
 display(map_korea)
-
-
-# In[ ]:
 
 
 
